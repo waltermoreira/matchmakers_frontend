927350dae95b86a56814a58c51af8a7c690e72bc
Walter Moreira <walter@waltermoreira.net>
2016-12-16 12:26:12 -0600

    Update for new columns

--- app/app.py
+++ app/app.py
@@ -16,22 +16,6 @@ api = Api(app)
 TOKEN = 'apasaveslives'
 
 
-def row_to_dict(row):
-    d = {
-        'Name': row[1],
-        'Notes': row[8],
-        'Scores': {
-            'Dog': row[3],
-            'Child': row[4],
-            'Cat': row[5],
-            'Home': row[6]
-        },
-        'Shelterluv data': row[2] != '',
-        'Updated': row[0]
-    }
-    return d
-
-
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
@@ -46,10 +30,33 @@ class Matchmaker(Resource):
         self.sheet = self.client.open_by_key(
             '1huASrSqMFRqfSgVLpq06JJxxEIRDOMOR9T_R9Vx5vXU')
         self.worksheet = self.sheet.worksheet_by_title('MMKL')
+#        self.get_column_names()
+
+    def get_column_names(self):
+        header = self.worksheet.row(4)
+        for i, item in enumerate(header):
+            if 'matchmaker notes' in item.lower():
+                self.notes = i
+                return
         
+    def row_to_dict(self, row):
+        d = {
+            'Name': row[1],
+            'Notes': row[-1],
+            'Scores': {
+                'Dog': row[3],
+                'Child': row[4],
+                'Cat': row[5],
+                'Home': row[6]
+            },
+            'Shelterluv data': row[2] != '',
+            'Updated': row[0]
+        }
+        return d
+
     def get(self):
         values = self.worksheet.all_values()[4:]
-        return [row_to_dict(row) for row in values]
+        return [self.row_to_dict(row) for row in values]
 
         
 api.add_resource(HelloWorld, '/')
